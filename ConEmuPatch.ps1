345af9c624178849b6514d5e2d6bbfabceb950f9
friedrich <friedrich.brunzema@sciex.com>
2019-12-27 18:26:04 -0500

    Install vscode extensions

--- ConEmuPatch.ps1
+++ ConEmuPatch.ps1
@@ -19,5 +19,27 @@ if (-not (Test-Path -Path 'C:\Program Files\ConEmu' -PathType Container))
 
 Write-Host "Copying to C:\Program Files\ConEmu\ConEmu.xml"
 Copy-Item ConEmu.xml -Destination 'C:\Program Files\ConEmu' -Force
+
+Write-Host "Installing useful Visual Studio Code extensions"
+&code --install-extension ajhyndman.jslint
+&code --install-extension alefragnani.Bookmarks
+&code --install-extension bibhasdn.unique-lines
+&code --install-extension DotJoshJohnson.xml
+&code --install-extension eamodio.gitlens
+&code --install-extension eriklynd.json-tools
+&code --install-extension jebbs.plantuml
+&code --install-extension Leopotam.csharpfixformat
+&code --install-extension marp-team.marp-vscode
+&code --install-extension mauve.terraform
+&code --install-extension mblode.pretty-formatter
+&code --install-extension mindginative.terraform-snippets
+&code --install-extension ms-azuretools.vscode-docker
+&code --install-extension ms-vscode-remote.remote-wsl
+&code --install-extension ms-vscode.csharp
+&code --install-extension ms-vscode.powershell
+&code --install-extension slevesque.vscode-hexdump
+&code --install-extension tht13.html-preview-vscode
+&code --install-extension Tyriar.sort-lines
+&code --install-extension yuichinukiyama.TabSpacer
 Write-Host "Done"
 
